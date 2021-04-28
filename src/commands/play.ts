5b12a19979354fcaebe031c5d51816a1e689b039
alalfakawma <alalfakawma@live.com>
2021-04-28 20:42:18 +0530

    fix: Fixed not checking if the first argument is an url or not before checking for the validateUrl method.

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -10,13 +10,15 @@ module.exports = {
 
         if (!voiceChannel) return msg.channel.send('Voice channel ah i awm angai! Mimawl!');
 
+        const isURL = new RegExp(/^(http|https).*\.[a-z]{2,}/);
+
         if (!args[0] && !queue.length) return msg.channel.send("Link chuuu! Mimawl!");
         else if (args[0]) {
-            if (!ytdl.validateURL(args[0])) {
+            if (!isURL.test(args[0])) {
                 const song: { title: string, url: string } = await new Promise((resolve, _rej) => {
                     search(args.join(' '), (err, res) => {
                         if (err) {
-                            return msg.channel.send("🛑 Youtube link a nilo tlat mai! Ti tha leh rawh!");
+                            return msg.channel.send("🛑 Diklo a awm meks, ti tha leh chhin rawh!");
                         }
 
                         resolve({
@@ -27,11 +29,15 @@ module.exports = {
                 });
                 queue.push(song);
             } else {
-                const songInfo = (await ytdl.getInfo(args[0])).videoDetails;
-                queue.push({
-                    title: songInfo.title,
-                    url: args[0],
-                });
+                if (ytdl.validateURL(args[0])) {
+                    const songInfo = (await ytdl.getInfo(args[0])).videoDetails;
+                    queue.push({
+                        title: songInfo.title,
+                        url: args[0],
+                    });
+                } else {
+                    return msg.channel.send("🛑 Youtube link a nilo tlat mai, ti tha leh rawh!");
+                }
             }
 
             if (queue.length > 1) msg.channel.send(`👍 **Added to queue:** ${queue[queue.length - 1].title}`);
