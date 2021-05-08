d3a39bce53b5fcc5d45fcf7bb46d743d5457a4c7
alalfakawma <alalfakawma@live.com>
2021-05-08 12:15:00 +0530

    fix: First stop song that is playing then clear queue.

--- src/commands/clearqueue.ts
+++ src/commands/clearqueue.ts
@@ -4,6 +4,10 @@ import { queue } from '../index';
 module.exports = {
     name: 'clearqueue',
     callback: (msg: Message, _args: Array<string>) => {
+        const [ song ] = queue;
+        if (song.dispatcher) {
+            song.dispatcher.destroy();
+        }
         queue.length = 0;
         msg.channel.send('🚮 Queue cleared!');
     },
