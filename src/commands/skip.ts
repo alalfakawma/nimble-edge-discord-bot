5b8585b1168c3210bc8d9d447dc14de101e6f16d
alalfakawma <alalfakawma@live.com>
2021-05-08 11:51:35 +0530

    fix: Removed skip message.

--- src/commands/skip.ts
+++ src/commands/skip.ts
@@ -3,8 +3,7 @@ import { queue } from '../index';
 
 module.exports = {
     name: 'skip',
-    callback: (msg: Message, _args: Array<string>) => {
+    callback: (_msg: Message, _args: Array<string>) => {
         queue[0].dispatcher?.end();
-        msg.channel.send("⏩ Skip meks!!");
     },
 };
