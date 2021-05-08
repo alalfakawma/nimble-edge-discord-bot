a6fb3f3d46b080820c46c3940bd98c531844ee55
alalfakawma <alalfakawma@live.com>
2021-05-08 12:11:43 +0530

    fix: Added embed message to queue.

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -1,22 +1,31 @@
 import { Message } from 'discord.js';
 import { queue } from '../index';
+import { MessageEmbed } from 'discord.js';
+import { title } from 'node:process';
 
 module.exports = {
     name: 'queue',
     callback: (msg: Message, _args: Array<string>) => {
         const queueList = queue.map(
-            (item, index) => `**${(index + 1)}.** ${item.title} ${ (item.dispatcher) ? '** - (Playing)**' : '' }`
+            (item, index) => `**${(index + 1)}.** ${item.title} ${ (item.dispatcher && !item.dispatcher.paused) ? '** - (Playing)**' : '' }`
         );
+
+        const embed = new MessageEmbed()
+            .setColor('#FBAB81')
+            .setTitle('Song Queue:')
+            .setDescription('Showing first 25 songs.')
 
         if (queueList.length) {
             if (queueList.length > 25) {
                 const first25 = queueList.slice(0, 25);
-                msg.channel.send(`**First 25 songs..**\n${first25.join('\n')}`);
+                embed.addField('----', first25.join('\n'));
+                msg.channel.send(embed);
             } else {
-                msg.channel.send(queueList.join('\n'));
+                embed.addField('----', queueList.join('\n'));
+                msg.channel.send(embed);
             }
         } else {
-            msg.channel.send("Queue ah hla a awmlo!");
+            msg.channel.send("🤷 Queue ah hla a awmlo!");
         }
     },
 };
